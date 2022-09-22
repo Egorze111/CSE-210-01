2f5e6a1f72ebb348c6c48799844a7119dd80fc59
Equacken111 <e.quackie16@gmail.com>
2022-09-22 16:23:18 -0600

    Group work
    Co-authored-by: Andre Regino  <regino-pan-a-i@users.noreply.github.com>
    Co-authored-by: DemascusSteel <DemascusSteel@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,6 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/////////////////////////////
+// Assignment 1: create a game of Tic-Tac-Toe
+// Group Members: Emma Quackenbush, Nathan Marble, Justin Paystrup, Andre Regino, Madison Brown
+////////////////////////////
+
+Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+//variables
+string first = "1";
+string second = "2";
+string third = "3";
+string fourth = "4";
+string fifth = "5";
+string sixth = "6";
+string seven = "7";
+string eight = "8";
+string nine = "9";
+//
 
 // interface start
-    Console.WriteLine("first line of board");
+
+    Console.WriteLine($"{first}|{second}|{third}");
+    Console.WriteLine("-+-+-");
+    Console.WriteLine($"{fourth}|{fifth}|{sixth}");
+    Console.WriteLine("-+-+-");
+    Console.WriteLine($"{seven}|{eight}|{nine}");
+    
 // interface end
+
+// user input start
+
+Console.WriteLine('Please choose "x" or "o"');
+string symbol = Console.ReadLine();
+Console.WriteLine('Choose a number from the grid');
+string userInput = Console.ReadLine();
+
+if (userInput == first)
+{
+    first = userInput;    
+}
+else if (userInput = second)
+{
+    second = userInput;
+}
+else if (userInput = third)
+{
+    third = userInput;
+}
+else if (userInput = fourth)
+{
+    fourth = userInput;
+}
+else if (userInput = fifth)
+{
+    fifth = userInput;
+}
+else if (userInput = sixth)
+{
+    sixth = userInput;
+}
+else if (userInput = seven)
+{
+    seven = userInput;
+}
+else if (userInput = eight)
+{
+    eight = userInput;
+}
+else if (userInput = nine)
+{
+    nine = userInput;
+}
+
+
+
+
